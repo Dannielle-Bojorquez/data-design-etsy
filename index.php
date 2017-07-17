e35cd57bffb9b9524ad6bc4f77fea6bba1a74363
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-17 15:39:43 -0600

    updated image tag

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8"/>
 		<title>data-design-etsy</title>
 	</head>
 	<body>
 	<p>
 		<h1>Persona and Demographics</h1>
-		<img src="epic/image/jen.jpg"/>
+		<img src="epic/image/jen.jpg" alt="jen"/>
 		<p>Jen is the an eutrapenur. Who makes custom furniture for Tiny House Living. She likes to build things with her
 			own hands that are made to her customer's needs. She likes that her products are personal and not products that
 			are mass produced and pushed out by the thousands. She loves the customization of every new piece that she makes.
@@ -19,9 +20,9 @@
 		<li>Location: The world</li>
 		<li>Character: The Builder</li>
 	</ul>
-		<h2>user case</h2>
+		<h2>User Case</h2>
 		<p>Jen's goal is to put her work out there for purchase</p>
-		<h3>Interaction Flow</h3>
+		<h2>Interaction Flow</h2>
 			<ol>
 			<li>Jen opens her web browser</li>
 			<li>types in the web address</li>
@@ -32,7 +33,7 @@
 			<li>reviews the post</li>
 			<li>submits post</li>
 		</ol>
-		<h4>Conceptual Model</h4>
+		<h2>Conceptual Model</h2>
 		<ul>
 			<li>ProfileId</li>
 			<li>Profile Activation Token</li>
@@ -42,14 +43,14 @@
 			<li>Profile Phone</li>
 			<li>Profile salt</li>
 		</ul>
-		<h5>Item Submission</h5>
+		<h3>Item Submission</h3>
 		<ul>
 			<li>Account Id</li>
 			<li>product photo</li>
 			<li>product description</li>
 			<li>date of post</li>
 		</ul>
-		<h6>purchase</h6>
+		<h3>purchase</h3>
 		<ul>
 			<li>quantity</li>
 			<li>payment source</li>
@@ -58,12 +59,11 @@
 			<li>payment submission</li>
 			<li>confirmation number</li>
 		</ul>
-		<h6>Relations</h6>
+		<h2>Relations</h2>
 		<ul>
 			<li>multiple users can comment on multiple items</li>
 			<li>multiple users can order multiple items</li>
 			<li>multiple users can chose multiple ways to pay</li>
-
 		</ul>
 		</body>
 </html>

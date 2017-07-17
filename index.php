280b5b1a77ffc0d183e5cf37e0780f553346fb74
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-17 13:07:20 -0600

    persona update

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>data-design-etsy</title>
+	</head>
+	<body>
+	<p>
+		<h1>Persona and Demographics</h1>
+		<img src="epic/image/jen.jpg"/>
+		<p>Jen is the an eutrapenur. Who makes custom furniture for Tiny House Living. She likes to build things with her
+			own hands that are made to her customer's needs. She likes that her products are personal and not products that
+			are mass produced and pushed out by the thousands. She loves the customization of every new piece that she makes.
+			Her own form of art work that others may enjoy.</p>
+	<ul>
+		<li>Name: Jen Watson</li>
+		<li>Age:32</li>
+		<li>Work: custom furniture</li>
+		<li>Family: Single</li>
+		<li>Location: The world</li>
+		<li>Character: The Builder</li>
+	</ul>
+		<h3>user case</h3>
+		<p>Jen's goal is to show more people her work</p>
+			<ol>
+			<li>Jen opens her web browser</li>
+			<li>types in the web address</li>
+			<li>clicks sign in button</li>
+			<li>enters login info</li>
+			<li>loads a picture of her latest creation</li>
+			<li>puts in a discription and price</li>
+			<li>reviews the post</li>
+			<li>submits post</li>
+		</ol>
+
+		</body>
+</html>
